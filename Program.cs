f227668616ce61fb0f59601fd9639b38a9a898b8
Aston Arnott (1000122523) <ARNOAO1@student.op.ac.nz>
2024-05-14 14:03:46 +1200

    Aston Do While Loop

--- Program.cs
+++ Program.cs
@@ -9,8 +9,15 @@
 
             Console.WriteLine("Hello, World!");
 			Console.WriteLine("123 123 123 Aston Code Line 123 123 123");
+			/* Aston Code */
+			int astoncount = 0;
+			do
+			{
+				Console.WriteLine($"Aston Count Num: {astoncount}");
+			}while astoncount != 10
 
             Console.Writeline("Jays Turn!!")
+            Console.ReadLine();
 
         }
     }
