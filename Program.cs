ff73a1d0aea93831bd47900c58c65a82bb97a1a7
Hazel Mapuranga (1000125469) <MAPUHN1@student.op.ac.nz>
2024-05-14 14:05:59 +1200

    Upload to repo

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@
         {
 
             Console.WriteLine("Hello, my name is Hazel");
+            // Hazels Code line    
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
 
             Console.WriteLine("Hello, World!");
 			Console.WriteLine("123 123 123 Aston Code Line 123 123 123");
