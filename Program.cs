2c89be5f7029f729a518aeecf378b88ceaf2c59b
Aston Arnott (1000122523) <ARNOAO1@student.op.ac.nz>
2024-05-14 14:12:36 +1200

    Fixed Aston Code

--- Program.cs
+++ Program.cs
@@ -4,19 +4,18 @@
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Hello, my name is Hazel");
-
             Console.WriteLine("Hello, World!");
-			Console.WriteLine("123 123 123 Aston Code Line 123 123 123");
-			/* Aston Code */
-			int astoncount = 0;
-			do
-			{
-				Console.WriteLine($"Aston Count Num: {astoncount}");
-			}while astoncount != 10
-
-            Console.Writeline("Jays Turn!!")
+            Console.WriteLine("123 123 123 Aston Code Line 123 123 123");
+            /* Aston Code */
+            int astoncount = 0;
+            do
+            {
+                Console.WriteLine($"Aston Count Num: {astoncount}");
+                astoncount++;
+                Thread.Sleep(1000);
+            } while (astoncount != 10);
+            Console.WriteLine("Jays Turn!!");
             Console.ReadLine();
 
         }
